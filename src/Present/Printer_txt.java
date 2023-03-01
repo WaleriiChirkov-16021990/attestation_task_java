0eb5d083c374d9acf50619bacbdbcdb9186a1e02
Валерий Чирков <zx84058405@live.com>
2023-03-02 00:12:13 +0300

    Create Presenter_txt & User_input class

---
+++ src/Present/Printer_txt.java
@@ -0,0 +1,12 @@
+package Present;
+
+public class Printer_txt {
+	private String text;
+	
+	public Printer_txt(String text) {
+		this.text = text;
+	}
+	public void print(){
+		System.out.println(this.text);
+	}
+}
---
+++ src/UI/Input_gamer.java
@@ -0,0 +1,46 @@
+package UI;
+
+import Present.Printer_txt;
+
+import java.util.Scanner;
+
+public class Input_gamer {
+	private String command;
+	private Scanner scanner;
+	private String input;
+	
+	public Input_gamer(String command) {
+		this.command = command;
+		this.scanner = new Scanner(System.in);
+	}
+	
+	public String get_input(){
+		new Printer_txt(this.getCommand()).print();
+		this.setInput(this.getScanner().nextLine());
+		return this.getInput();
+	}
+	
+	public String getCommand() {
+		return command;
+	}
+	
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+	public Scanner getScanner() {
+		return scanner;
+	}
+	
+	public void setScanner(Scanner scanner) {
+		this.scanner = scanner;
+	}
+	
+	public String getInput() {
+		return input;
+	}
+	
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
